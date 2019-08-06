0ebe4412639e8e57b48e9ed3910b07ab7baa5aa9
kenwrites <28812088+kenwrites@users.noreply.github.com>
2019-08-06 11:44:55 -0400

    Completed ChurchService methods

--- NextMassConsole/ChurchService.cs
+++ NextMassConsole/ChurchService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace NextMassConsole.Model
@@ -9,39 +10,59 @@ namespace NextMassConsole.Model
         private NextMassContext _db = new NextMassContext();
         public void AddChurch(IChurch church)
         {
-            //_db.Churches.Add(new Church(church));
+            _db.Churches.Add(new Church(church));
+            _db.SaveChanges();
         }
+        // TODO:  add options to update only MassTimes, Name, or Coordinates
         public void UpdateChurch(int churchId, IChurch updatedChurch)
         {
-            //Church churchToUpdate = _db.Churches.Find(churchId);
-            //churchToUpdate = new Church(updatedChurch);
+            Church churchToUpdate = _db.Churches.Find(churchId);
+            churchToUpdate = new Church(updatedChurch);
         }
         public void DeleteChurch(Church church)
         {
-            //_db.Churches.Remove(church);
+            _db.Churches.Remove(church);
+            _db.SaveChanges();
         }
         public void DeleteChurch(int churchId)
         {
+            _db.Churches.Remove(_db.Churches.Find(churchId));
+            SaveChanges();
         }
+        // TODO:  implement regex or string.contains.
         public IChurch GetChurch(string name)
         {
-            return new Church();
+            return _db.Churches.First
+            (
+                c => c.Name.ToLower()
+                .Contains(name.ToLower())
+            );
         }
         public IChurch GetChurch(int Id)
         {
-            return new Church();
+            return _db.Churches.First(c => c.Id == Id);
         }
         public ICollection<Church> GetNearbyChurches(ILocation location, int distanceInMiles = 10)
         {
-            return new List<Church>();
+            throw new NotImplementedException();            
         }
-        public ICollection<IChurch> ReturnAllChurchesForTesting()
+        public void RemoveAllChurchesForTesting()
         {
-            return (ICollection<IChurch>) new List<Church>();
+            _db.Churches.RemoveRange(
+                _db.Churches.ToArray());
+            _db.SaveChanges();
+        }
+        public ICollection<Church> ReturnAllChurchesForTesting()
+        {
+            return _db.Churches.ToList();
+        }
+        public bool AnyChurches()
+        {
+            return _db.Churches.Any();
         }
         public void SaveChanges()
         {
-            //_db.SaveChanges();
+            _db.SaveChanges();
         }
     }
 }
--- NextMassConsoleTests/ChurchServiceTests.cs
+++ NextMassConsoleTests/ChurchServiceTests.cs
@@ -2,6 +2,7 @@
 using NextMassConsoleTests.Mocks;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace NextMassConsoleTests
@@ -11,49 +12,28 @@ namespace NextMassConsoleTests
         private ChurchService _churchService = new ChurchService();
 
         [Theory]
-        [InlineData(84.22322, 23.22344, "Saturday", 9, 30, "Saturday", 11, 30)]
-        [InlineData(24.44932, 25.224343, "Sunday", 7, 00, "Sunday", 9, 00)]
-        public void AddChurch_MockChurch_Tests(double lattitude, double longitude, string day1, int hour1, int minute1, string day2, int hour2, int minute2)
+        [InlineData(84.22322, 23.22344, "Saturday", 9, 30, "Saturday", 11, 30, "Saint Testy McTesterton")]
+        [InlineData(24.44932, 25.224343, "Sunday", 7, 00, "Sunday", 9, 00, "Saint Testy McTesterton2")]
+        public void AddChurch_Church_Tests(double lattitude, double longitude, string day1, int hour1, int minute1, string day2, int hour2, int minute2, string churchName)
         {
-            var churchLoc = new MockLocation { Latitude = lattitude, Longitude = longitude };
+            _churchService.RemoveAllChurchesForTesting();
+            var churchLoc = new Location { Latitude = lattitude, Longitude = longitude };
             var massTimes = new List<IMassTime>
             {
-                new MockMassTime { Day = Enum.Parse<DayOfWeek>(day1), Hour = hour1, Minute = minute1 },
-                new MockMassTime { Day = Enum.Parse<DayOfWeek>(day2), Hour = hour2, Minute = minute2 },
+                new MassTime { Day = Enum.Parse<DayOfWeek>(day1), Hour = hour1, Minute = minute1 },
+                new MassTime { Day = Enum.Parse<DayOfWeek>(day2), Hour = hour2, Minute = minute2 },
             };
-            var expected = new MockChurch { Coordinates = churchLoc, MassTimes = massTimes};
-            var actualListBefore = (List<IChurch>)_churchService.ReturnAllChurchesForTesting();
-
-            _churchService.AddChurch(expected);
-
-            var actualListAfter = _churchService.ReturnAllChurchesForTesting();
-
-            Assert.True(
-                actualListAfter.Count == actualListBefore.Count + 1 && 
-                actualListAfter.Contains(expected));
-        }
-
-        [Theory]
-        [InlineData(84.22322, 23.22344, "Saturday", 9, 30, "Saturday", 11, 30)]
-        [InlineData(24.44932, 25.224343, "Sunday", 7, 00, "Sunday", 9, 00)]
-        public void AddChurch_Church_Tests(double lattitude, double longitude, string day1, int hour1, int minute1, string day2, int hour2, int minute2)
-        {
-            var churchLoc = new MockLocation { Latitude = lattitude, Longitude = longitude };
-            var massTimes = new List<IMassTime>
-            {
-                new MockMassTime { Day = Enum.Parse<DayOfWeek>(day1), Hour = hour1, Minute = minute1 },
-                new MockMassTime { Day = Enum.Parse<DayOfWeek>(day2), Hour = hour2, Minute = minute2 },
-            };
-            var expected = new Church { Coordinates = churchLoc, MassTimes = massTimes };
+            var expected = new Church { Coordinates = churchLoc, MassTimes = massTimes, Name = churchName };
             var actualListBefore = (List<Church>)_churchService.ReturnAllChurchesForTesting();
 
             _churchService.AddChurch(expected);
 
-            var actualListAfter = _churchService.ReturnAllChurchesForTesting();
+            var actualListAfter = (List<Church>)_churchService.ReturnAllChurchesForTesting();
 
             Assert.True(
                 actualListAfter.Count == actualListBefore.Count + 1 &&
-                actualListAfter.Contains(expected));
+                actualListAfter.Find(c => c.Name == expected.Name)
+                .MassTimes == expected.MassTimes);
         }
 
         [Theory]
@@ -61,15 +41,13 @@ namespace NextMassConsoleTests
         [InlineData(21.362920, 67.443020)]
         public void UpdateChurch_Location_Tests(double lattitude, double longitude)
         {
-            var actualListBefore = (List<Church>)_churchService.ReturnAllChurchesForTesting();
-
             // If no churches in DbSet, add some
-            if (actualListBefore.Count == 0)
+            if (!_churchService.AnyChurches())
             {
                 AddSomeChurches();
-                actualListBefore = (List<Church>)_churchService.ReturnAllChurchesForTesting();
             }
 
+            var actualListBefore = (List<Church>)_churchService.ReturnAllChurchesForTesting();
             var churchToUpdate = actualListBefore.Find(c => c is IChurch);
             var updatedLoc = new MockLocation { Latitude = lattitude, Longitude = longitude };
             churchToUpdate.Coordinates = updatedLoc;
@@ -77,11 +55,9 @@ namespace NextMassConsoleTests
             _churchService.UpdateChurch(churchToUpdate.Id, churchToUpdate);
 
             var actualListAfter = (List<Church>)_churchService.ReturnAllChurchesForTesting();
-
             var actual = actualListAfter.Find(c => c.Id == churchToUpdate.Id);
 
             Assert.Equal(churchToUpdate.Coordinates, actual.Coordinates);
-
         }
 
         [Theory]
@@ -89,15 +65,13 @@ namespace NextMassConsoleTests
         [InlineData("Saturday", 5, 30, "Sunday", 8, 00)]
         public void UpdateChurch_MassTimes_Tests(string day1, int hour1, int minute1, string day2, int hour2, int minute2)
         {
-            var actualListBefore = (List<Church>)_churchService.ReturnAllChurchesForTesting();
-
             // If no churches in DbSet, add one
-            if (actualListBefore.Count == 0)
+            if (!_churchService.AnyChurches())
             {
                 AddSomeChurches();
-                actualListBefore = (List<Church>)_churchService.ReturnAllChurchesForTesting();
             }
 
+            var actualListBefore = (List<Church>)_churchService.ReturnAllChurchesForTesting();
             var churchToUpdate = actualListBefore.Find(c => c is IChurch);
             var newMassTimes = new List<IMassTime>
             {
@@ -112,35 +86,28 @@ namespace NextMassConsoleTests
             var actual = actualListAfter.Find(c => c.Id == churchToUpdate.Id);
 
             Assert.Equal(churchToUpdate.MassTimes, actual.MassTimes);
-
         }
 
         [Fact]
         public void DeleteChurch_FirstChurch_Test()
         {
-            var actualListBefore = (List<Church>)_churchService.ReturnAllChurchesForTesting();
+            _churchService.RemoveAllChurchesForTesting();
+            AddSomeChurches();
             
-            // If no churches in DbSet, add some
-            if (actualListBefore.Count == 0)
-            {
-                AddSomeChurches();
-                actualListBefore = (List<Church>)_churchService.ReturnAllChurchesForTesting();
-            }
-
+            var actualListBefore = (List<Church>)_churchService.ReturnAllChurchesForTesting();
             var churchToDelete = actualListBefore.Find(c => c is IChurch);
 
             _churchService.DeleteChurch(churchToDelete);
 
             var actualListAfter = (List<Church>)_churchService.ReturnAllChurchesForTesting();
-
             Assert.True(actualListAfter.Count == actualListBefore.Count - 1 &&
                 !actualListAfter.Contains(churchToDelete));
-
         }
 
         [Fact]
         public void DeleteChurch_LastChurchWithDuplicates_Test()
         {
+            _churchService.RemoveAllChurchesForTesting();
             AddSomeChurches();
             var actualListBefore = (List<Church>)_churchService.ReturnAllChurchesForTesting();
             var churchToDelete = actualListBefore.FindLast(c => c is IChurch);
@@ -155,6 +122,7 @@ namespace NextMassConsoleTests
         [Fact]
         public void DeleteChurch_ChurchInMiddleWithDuplicates_Test()
         {
+            _churchService.RemoveAllChurchesForTesting();
             AddSomeChurches();
             var actualListBefore = (List<Church>)_churchService.ReturnAllChurchesForTesting();
             var churchToDelete = actualListBefore.Find(c => c.Coordinates.Latitude == 18.34432);
@@ -174,6 +142,7 @@ namespace NextMassConsoleTests
         [InlineData("TestIdenticalChurch")]
         public void DeleteChurch_ChurchId_Test(string churchName)
         {
+            _churchService.RemoveAllChurchesForTesting();
             AddSomeChurches();
             var actualListBefore = (List<Church>)_churchService.ReturnAllChurchesForTesting();
             var churchToDelete = actualListBefore.Find(c => c.Name == churchName);
@@ -193,6 +162,7 @@ namespace NextMassConsoleTests
         [InlineData("TestIdenticalChurch")]
         public void GetChurch_ByName_Tests(string churchName)
         {
+            _churchService.RemoveAllChurchesForTesting();
             AddSomeChurches();
             var actual = _churchService.GetChurch(churchName);
 
@@ -208,6 +178,7 @@ namespace NextMassConsoleTests
         [InlineData("TestIdenticalChurch")]
         public void GetChurch_ById_Tests(string churchName)
         {
+            _churchService.RemoveAllChurchesForTesting();
             AddSomeChurches();
             var actualList = (List<Church>)_churchService.ReturnAllChurchesForTesting();
             var expected = actualList.Find(c => c.Name == churchName);
@@ -221,40 +192,40 @@ namespace NextMassConsoleTests
 
         private void AddSomeChurches()
         {
-            var churchLoc = new MockLocation { Latitude = 21.85843, Longitude = 32.44373 };
+            var churchLoc = new Location { Latitude = 21.85843, Longitude = 32.44373 };
             var massTimes = new List<IMassTime>
             {
-                new MockMassTime { Day = DayOfWeek.Saturday, Hour = 5, Minute = 30 },
-                new MockMassTime { Day = DayOfWeek.Sunday, Hour = 7, Minute = 30 },
+                new MassTime { Day = DayOfWeek.Saturday, Hour = 5, Minute = 30 },
+                new MassTime { Day = DayOfWeek.Sunday, Hour = 7, Minute = 30 },
             };
             var newChurch = new Church { Coordinates = churchLoc, MassTimes = massTimes, Name = "Test1" };
             _churchService.AddChurch(newChurch);
 
-            churchLoc = new MockLocation { Latitude = 31.33223, Longitude = 62.4432234 };
+            churchLoc = new Location { Latitude = 31.33223, Longitude = 62.4432234 };
             massTimes = new List<IMassTime>
             {
-                new MockMassTime { Day = DayOfWeek.Saturday, Hour = 19, Minute = 30 },
-                new MockMassTime { Day = DayOfWeek.Sunday, Hour = 9, Minute = 00 },
+                new MassTime { Day = DayOfWeek.Saturday, Hour = 19, Minute = 30 },
+                new MassTime { Day = DayOfWeek.Sunday, Hour = 9, Minute = 00 },
             };
             newChurch = new Church { Coordinates = churchLoc, MassTimes = massTimes, Name = "Test2" };
             _churchService.AddChurch(newChurch);
 
             // identical church 1
-            churchLoc = new MockLocation { Latitude = 18.34432, Longitude = 43.223423 };
+            churchLoc = new Location { Latitude = 18.34432, Longitude = 43.223423 };
             massTimes = new List<IMassTime>
             {
-                new MockMassTime { Day = DayOfWeek.Saturday, Hour = 20, Minute = 30 },
-                new MockMassTime { Day = DayOfWeek.Sunday, Hour = 11, Minute = 00 },
+                new MassTime { Day = DayOfWeek.Saturday, Hour = 20, Minute = 30 },
+                new MassTime { Day = DayOfWeek.Sunday, Hour = 11, Minute = 00 },
             };
             newChurch = new Church { Coordinates = churchLoc, MassTimes = massTimes, Name = "TestIdenticalChurch" };
             _churchService.AddChurch(newChurch);
 
             // identical church 2
-            churchLoc = new MockLocation { Latitude = 18.34432, Longitude = 43.223423 };
+            churchLoc = new Location { Latitude = 18.34432, Longitude = 43.223423 };
             massTimes = new List<IMassTime>
             {
-                new MockMassTime { Day = DayOfWeek.Saturday, Hour = 20, Minute = 30 },
-                new MockMassTime { Day = DayOfWeek.Sunday, Hour = 11, Minute = 00 },
+                new MassTime { Day = DayOfWeek.Saturday, Hour = 20, Minute = 30 },
+                new MassTime { Day = DayOfWeek.Sunday, Hour = 11, Minute = 00 },
             };
             newChurch = new Church { Coordinates = churchLoc, MassTimes = massTimes, Name = "TestIdenticalChurch" };
             _churchService.AddChurch(newChurch);        
